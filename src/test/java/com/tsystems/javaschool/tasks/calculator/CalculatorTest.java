24eafca3444965425274f1befa55eca73836d595
GetRhymes <tarasenko.ns@edu.spbstu.ru>
2020-12-01 13:34:01 +0300

    codeship

--- src/test/java/com/tsystems/javaschool/tasks/calculator/CalculatorTest.java
+++ src/test/java/com/tsystems/javaschool/tasks/calculator/CalculatorTest.java
@@ -266,10 +266,4 @@ public class CalculatorTest {
         //assert
         Assert.assertEquals(expectedResult, result);
     }
-    @Test
-        public void evaluate20() {
-            String input = "1+2*(3+3*(4+4)+3)";
-            String result = calc.evaluate(input);
-        }
-
 }
